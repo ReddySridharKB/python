9479115d78f2eadd6a682a50872e748ac305be2c
Reddy Sridhar K B <reddysridharkb@gmail.com>
2026-08-25 19:55:57 +0530

    module

---
+++ module.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.sqrt(16))      # Square root
+print(math.pow(2, 3))     # 2³
+print(math.factorial(5))  # 5!
+print(math.ceil(4.2))     # Round up
+print(math.floor(4.8))    # Round down
