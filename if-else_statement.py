cc8cb44f78bcfd197df28ac22adebff62a307e41
Reddy Sridhar K B <reddysridharkb@gmail.com>
2026-08-26 15:08:28 +0530

    if-elsestatements

---
+++ if-else_statement.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+print("Bye bye......")
