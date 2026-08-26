22d70ae2b9dec1af84a85241fc850413984c5a3c
Reddy Sridhar K B <reddysridharkb@gmail.com>
2026-08-26 22:43:48 +0530

    tuple modify

--- tuple.py
+++ tuple.py
@@ -20,3 +20,14 @@ print(tupB)  # Output: (34, 'sridha', [1, 17, 3, 4])
 
 print(34 in tupB)  # Output: True
 print(1 in tupB)  # Output: True
+
+my_tuple = (1, 2, 3, 4, 5, 1)
+
+print(my_tuple[0])      # indexing
+print(my_tuple[-1])     # negative indexing
+print(my_tuple[1:3])    # slicing
+
+print(len(my_tuple))    # length
+
+print(my_tuple.count(1))   # count how many times 1 appears
+print(my_tuple.index(4))   # position of 4
