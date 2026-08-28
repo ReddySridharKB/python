b8bf1792d0c79ee1e62b74d592f62c174f7c83dc
Reddy Sridhar K B <reddysridharkb@gmail.com>
2026-08-28 22:49:57 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+cities = ["Bengaluru", "Chennai", "Hyderabad"]
+
+for city in cities:
+    print(city)
+
+print("--------------------")
+
+
+#string
+name = "Reddy"
+
+for letter in name:
+    print(letter)
+print("--------------------")
+
+for i in range(5):
+    print(i)
